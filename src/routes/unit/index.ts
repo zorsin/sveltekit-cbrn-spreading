ba97fd9b6323bad039f4cd8ba506e814482f9eab
S. Wirries <soeren-dev@swirries.de>
2022-06-24 15:33:41 +0200

    checks if running mission exists for units

--- src/routes/unit/index.ts
+++ src/routes/unit/index.ts
@@ -1,11 +1,28 @@
 import type { RequestHandler } from './__types/index';
+import { connect, close } from '$lib/logic/mongo';
 
 export const get: RequestHandler = async ({ locals }) => {
+  let missionUuid = null;
+  let unitUuid = locals.session.data?.unit?.uuid;
+  // check if the mission is available in the db
+  if (locals.session.data?.unit?.missionId) {
+    const cookieMission = locals.session.data.unit.missionId;
+    const collMission = await connect('mission');
+    const resMission = await collMission.findOne({ uuid: cookieMission });
+    close();
+    if (resMission) {
+      missionUuid = cookieMission;
+    } else {
+      unitUuid = null;
+      await locals.session.update(() => ({ unit: { uuid: null, missionId: null } }));
+    }
+  }
+
   return {
     status: 200,
     body: {
-      unitUuid: locals.session.data?.unit?.uuid,
-      missionUuid: locals.session.data?.unit?.missionId,
+      unitUuid,
+      missionUuid,
     },
   };
 };
