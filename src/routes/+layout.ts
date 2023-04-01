2f92879432797f922382d6b8417dcb0550ea830c
S. Wirries <soeren-dev@swirries.de>
2023-04-01 13:36:06 +0200

    removed twind

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,14 +1,14 @@
-import install from '@twind/with-sveltekit';
-import twindConfig from '$lib/twind.config';
+// import install from '@twind/with-sveltekit';
+// import twindConfig from '$lib/twind.config';
 import { init, waitLocale, getLocaleFromNavigator } from 'svelte-intl-precompile';
 import { registerAll } from '$locales';
 
-install(twindConfig);
+// install(twindConfig);
 registerAll();
 
 export const load = async () => {
   init({
-    fallbackLocale: 'en',
+    fallbackLocale: 'de', // TODO: change to en
     initialLocale: getLocaleFromNavigator(),
   });
   await waitLocale(); // awaits for initialLocale language pack to finish loading;
